e0c074c7399a4811a6e596eda4c1942b039995c8
bjzarn <zarny2010@yahoo.com>
2014-02-09 15:13:22 -0500

    core corssfit app
    added shell.java and shell_layout (contains application layout)

--- shell.java
+++ shell.java
@@ -1,2 +1,22 @@
-/* Java Code for the Login Shell */ 
+package com.example.crossfittracker;
 
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Shell extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.shell_layout);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.shell, menu);
+		return true;
+	}
+
+}
