e2b8b6703eaec89960fad12c4c40beacea55ecd2
bjzarn <zarny2010@yahoo.com>
2014-02-14 13:55:49 -0500

    Updated App Layout and Linking
    Linked app pages and passed variables

---
+++ Login.java
@@ -0,0 +1,71 @@
+package com.example.crossfittracker;
+//package ;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.support.v4.app.NavUtils;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
+
+public class Login extends Activity {
+
+
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        // Show the Up button in the action bar.
+        setupActionBar();
+    }
+
+    /**
+     * Set up the {@link android.app.ActionBar}, if the API is available.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private void setupActionBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+ //       getMenuInflater().inflate(R.menu.login, menu);
+        return true;
+    }
+    public void sendMessage(View view){
+    	Intent intent = new Intent (this, SendAuth.class);
+		
+		EditText editText = (EditText) findViewById(R.id.uname);
+		EditText editText2 = (EditText) findViewById(R.id.pwd);
+		String message = editText.getText().toString();
+		String message2 = editText2.getText().toString();
+		intent.putExtra(Shell.EXTRA_MESSAGE,message);
+		intent.putExtra(Shell.EXTRA_MESSAGE2, message2);
+		startActivity(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // This ID represents the Home or Up button. In the case of this
+                // activity, the Up button is shown. Use NavUtils to allow users
+                // to navigate up one level in the application structure. For
+                // more details, see the Navigation pattern on Android Design:
+                //
+                // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+                //
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
--- shell.java
+++ shell.java
@@ -1,10 +1,15 @@
 package com.example.crossfittracker;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class Shell extends Activity {
+	public final static String EXTRA_MESSAGE = "com.example.crossfittracker.MESSAGE";
+	public final static String EXTRA_MESSAGE2 = "com.example.crossfittracker.MESSAGE2";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,12 @@ public class Shell extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.shell, menu);
 		return true;
+	}
+
+	public void sendMessage(View view){
+		Intent intent = new Intent (this, Login.class);
+		startActivity(intent);
+		
 	}
 
 }
